b9b8954ed98113ce565773800ac6e994db6c26d7
marcelovelasquezherrera <marcelo.velasquez@rimac.com.pe>
2026-07-15 22:11:22 -0500

    Initial Commit

---
+++ UICibertec/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICibertec
+//
+//  Created by Marcelo Stefano Velasquez Herrera on 15/07/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
